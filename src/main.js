820985bb63013cb0f651c0303a3d66e745a0ef7c
Alfons-ES <alfonsersv@gmail.com>
2026-01-29 09:01:06 +0100

    loop & table

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+
+read()
+
 async function getRamschema() {
     const response = await fetch('https://webbutveckling.miun.se/files/ramschema.json');
     const data = await response.json();
@@ -10,12 +13,36 @@ async function read() {
     try {
         const data = await getRamschema();
         console.log(data);
+        write(data);
     } catch (error) {
         console.error("kan inte: ", error);
     }
 
-
 }
 
-getRamschema()
-read()
+function write(data) {
+    const outputEl = document.querySelector("#output");
+
+    outputEl.innerHTML = `
+    <h1>Webbutveckling - ramschema</h1>
+    <table>
+    <thead>
+    </thead>
+    <tbody>`
+
+    data.forEach(course => {
+        outputEl.innerHTML += `
+        <tr>
+        <td>${course.code}</td>
+        <td>${course.coursename}</td>
+        <td>${course.progression}</td>
+        <td><a href="${course.syllabus}" target="_blank">Kursplan</a></td>
+        </tr>
+    `
+    });
+
+    outputEl.innerHTML += `
+    </tbody>
+    </table>`
+
+}
