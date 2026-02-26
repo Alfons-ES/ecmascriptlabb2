02c61fa25a33d2325fdd00fe8b829e2a45603bc2
Alfons-ES <alfonsersv@gmail.com>
2026-02-26 14:30:51 +0100

    kartan laddas in och det finns ett inputfält

--- src/main.js
+++ src/main.js
@@ -337,3 +337,9 @@ function calcDisplay(data) {
 
 
 
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
